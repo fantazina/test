5807ae5f29c52c338bbd0695d7270580bd77ba2b
fantazina <137284904+fantazina@users.noreply.github.com>
2024-05-25 15:18:01 +0900

    first commit

--- write/joinPg.js
+++ write/joinPg.js
@@ -5,6 +5,7 @@ export default function Join(){
             <form action="/api/post/new2" method="POST">
                 <button type="submit">가입</button>
             </form>
+            // 잔디 심기 테스트 1 2 3 4 5 6 7 8 9 10
         </div>
     )
 }
